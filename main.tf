31ec8bdf6bb11d21565273fe8803858057bf247f
Josh Preston <joshuapreston@ucsb.edu>
2022-04-06 13:17:01 -0700

    added outputs

--- main.tf
+++ main.tf
@@ -20,3 +20,23 @@ resource "github_branch_protection" "branch" {
     required_approving_review_count   = var.required_pull_request_reviews.required_approving_review_count
   }
 }
+
+output "repo_id" {
+  value = github_branch_protection.branch.repository_id  
+}
+
+output "pattern" {
+  value = github_branch_protection.branch.pattern  
+}
+
+output "enforce_admins" {
+  value = github_branch_protection.branch.enforce_admins
+}
+
+output "push_restrictions" {
+  value = github_branch_protection.branch.push_restrictions
+}
+
+output "allows_force_pushes" {
+  value = github_branch_protection.branch.allows_force_pushes
+}
